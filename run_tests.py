ff443a827954a9827719518e1b5a39f2432abcf2
Athina Gouseti <athina.gouseti@outlook.com>
2021-09-26 15:51:42 +0100

    Tests for Classes

---
+++ run_tests.py
@@ -0,0 +1,9 @@
+import unittest
+ 
+from tests.driver_test import TestDriver
+from tests.team_test import TestTeam
+from tests.race_test import TestRace
+ 
+ 
+if __name__ == '__main__':
+    unittest.main()
