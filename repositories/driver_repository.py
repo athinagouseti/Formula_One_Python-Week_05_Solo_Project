178a9121b329b4450ad8c7f8d8e787cc3f7233b8
Athina Gouseti <athina.gouseti@outlook.com>
2021-09-26 22:42:42 +0100

    Driver & team Repositories

---
+++ repositories/driver_repository.py
@@ -0,0 +1,34 @@
+from db.run_sql import run_sql
+from models.driver import Driver
+from models.team import Team
+
+import repositories.team_repository as team_repository
+
+def save(driver):
+    sql = "INSERT INTO drivers (racing_number, name, nationality, team_id) VALUES (%s, %s, %s, %s)"
+    values = [driver.racing_number, driver.name, driver.nationality, driver.team.id]
+    results = run_sql(sql, values)
+    return driver
+
+def select_all():
+    drivers = []
+
+    sql = "SELECT * FROM drivers"
+    results = run_sql(sql)
+
+    for row in results:
+        team = team_repository.select(row['id'])
+        driver = Driver(row['racing_number'], row['name'], row['nationality'], team)
+        drivers.append(driver)
+    return drivers
+
+def delete(racing_number):
+    sql = "DELETE  FROM drivers WHERE racing_number = %s"
+    values = [racing_number]
+    run_sql(sql, values)
+
+
+def update(driver):
+    sql = "UPDATE drivers SET (racing_number, name, nationality, team_id) = (%s, %s, %s, %s, %s, %s) WHERE racing_number = %s"
+    values = [driver.racing_number, driver.name, driver.nationality, driver.team.id]
+    run_sql(sql, values)
---
+++ repositories/team_repository.py
@@ -0,0 +1,30 @@
+from db.run_sql import run_sql
+from models.driver import Driver
+from models.team import Team
+
+def save(team):
+    sql = "INSERT INTO teams (id, constructor, engine_supplier, chassis, engine_model, nationality) VALUES (%s, %s, %s, %s. %s, %s)"
+    values = [team.id, team.constructor, team.engine_supplier, team.chassis, team.engine_model, team.nationality]
+    results = run_sql(sql, values)
+    return team 
+
+def select(id):
+    team = None
+    sql = "SELECT * FROM teams WHERE id = %s"
+    values = [id]
+    result = run_sql(sql, values)[0]
+
+    if result is not None:
+        team = Team(result['id'], result['constructor'], result['engine_supplier'], result['chassis'], result['engine_model'], result['nationality'] )
+    return team
+
+def select_all():
+    teams = []
+
+    sql = "SELECT * FROM teams"
+    results = run_sql(sql)
+
+    for row in results:
+        team = Team(row['id'], row['constructor'], row['engine_supplier'], row['chassis'], row['engine_model'], row['nationality'] )
+        teams.append(teams)
+    return teams
