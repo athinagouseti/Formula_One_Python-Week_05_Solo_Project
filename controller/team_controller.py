99148a86f2ecb391e18ceb69cd22633494b6dfaa
Athina Gouseti <athina.gouseti@outlook.com>
2021-09-27 17:32:20 +0100

    New Team

--- controller/team_controller.py
+++ controller/team_controller.py
@@ -9,3 +9,20 @@ team_blueprint = Blueprint("teams", __name__)
 def teams():
     teams = team_repository.select_all()
     return render_template("teams/index.html", teams = teams)
+
+@team_blueprint.route("/teams/new", methods=['GET'])
+def new_team():
+    teams = team_repository.select_all()
+    return render_template("teams/new.html", teams = teams)
+
+@team_blueprint.route("/teams", methods = ['POST'])
+def add_team():
+    id = request.form["id"]
+    constructor = request.form["constructor"]
+    engine_supplier = request.form['engine_supplier']
+    chassis = request.form['chassis']
+    engine_model = request.form['engine_model']
+    nationality = request.form['nationality']
+    team = Team(id, constructor, engine_supplier, chassis, engine_model, nationality)
+    team_repository.save(team)
+    return redirect("/teams/new")
